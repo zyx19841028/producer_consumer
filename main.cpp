69ac6c075dc6485cac6db50a1fdccf490745c5f5
zhangyx <zyx19841028@163.com>
2018-05-22 21:28:52 +0800

    fix bugs

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #define PRODUCER 4
 #define CONSUMER 4
 sem_t sem_in,sem_out;
-pthread_mutex_t mutex_in,mutex_out;
+pthread_mutex_t mutex;
 pthread_t threads[PRODUCER + CONSUMER];
 RingQueue<int> rq;
 int productID;
@@ -16,7 +16,7 @@ void* HandleProducer(void* arg){
     int n = (int)arg;
     while(true){
         sem_wait(&sem_in);
-        pthread_mutex_lock(&mutex_in);
+        pthread_mutex_lock(&mutex);
         if(rq.IsFull()){
             printf("queue is full\n"); 
         }else{
@@ -24,7 +24,7 @@ void* HandleProducer(void* arg){
             printf("thread[%d] is producing %d\n",n, temp);
             rq.EnQueue(temp);
         }
-        pthread_mutex_unlock(&mutex_in);
+        pthread_mutex_unlock(&mutex);
         sem_post(&sem_out);
         sleep(1);
     }
@@ -35,14 +35,14 @@ void* HandleConsumer(void* arg){
     int* pq; 
     while(true){
         sem_wait(&sem_out);
-        pthread_mutex_lock(&mutex_out);
+        pthread_mutex_lock(&mutex);
         if(rq.IsEmpty()){
             printf("queue is empty\n"); 
         }else{
             pq = rq.DeQueue();
             printf("thread[%d] is consuming %d\n",n, *pq);
         }
-        pthread_mutex_unlock(&mutex_out);
+        pthread_mutex_unlock(&mutex);
         sem_post(&sem_in);
         sleep(1);
     }
@@ -52,6 +52,7 @@ int main(void){
     RingQueue<int>* q = new RingQueue<int>();
     sem_init(&sem_in, 0, q->GetCompacity());
     sem_init(&sem_out, 0, 0);
+    pthread_mutex_init(&mutex,NULL);
     for(int i = 0; i < PRODUCER; i++){
         pthread_create(&threads[i], NULL, HandleProducer, (void*)i);
     }
@@ -61,8 +62,7 @@ int main(void){
     for(int i = 0; i < CONSUMER+PRODUCER; i++){
         pthread_join(threads[i],NULL);
     }
-    pthread_mutex_destroy(&mutex_in);
-    pthread_mutex_destroy(&mutex_out);
+    pthread_mutex_destroy(&mutex);
     sem_destroy(&sem_in);
     sem_destroy(&sem_out);
 }
